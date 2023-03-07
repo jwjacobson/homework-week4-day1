6ee96612390058b040f826402eba06a924afdaa2
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-07 00:35:54 -0500

    Finish homework

--- homework_week4_day1.py
+++ homework_week4_day1.py
@@ -3,19 +3,15 @@
 # Exercise 1: filter out the blank strings
 places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
 
-filtered_places = list(filter(lambda place: place and place[0].isalpha(), places))
-print(filtered_places)
+print(list(filter(lambda place: place and place[0].isalpha(), places)))
 
 # Exercise 2: sort list by last name
 authors = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
-new_list = []
-# for name in authors:
-#     new_list.append(name.split())
-# print(new_list)
+
 new_list = list(map(lambda name: name.split(), authors))
 sorted_list = sorted(new_list, key=lambda name: name[-1].lower())
-joined_sorted_list = list(map(lambda name: ' '.join(name), sorted_list))
-print(joined_sorted_list)
+
+print(list(map(lambda name: ' '.join(name), sorted_list)))
 
 # Exercise 3: convert to fahrenheit
 # F = (9/5)*C + 32
@@ -24,9 +20,27 @@ places = [('Nashua',32),("Boston",12),("Los Angeles",44),("Miami",29)]
 def convert_to_fahrenheit(c):
     return (c * 9 / 5) + 32
 
-fahrenheit_list = list(map(lambda place: place[1] * 9/5 + 32, places))
-print(fahrenheit_list)
+print(list(map(lambda place: place[1] * 9/5 + 32, places)))
 
 # Exercise 4: Fibonacci up to specified member
+def fibonacci(num):
+    if num <= 1:
+        return num
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
 
-# Exercise 5: 
+def print_fibonacci(num):
+    for nums in range(num + 1):
+        print(f"Iteration {nums}: {fibonacci(nums)}")
+
+print_fibonacci(10)
+
+# Exercise 5: generator of squares down to 0
+def falling_squares(start, step=1):
+    stop = 0
+    while start > stop:
+        yield start**2
+        start -= step
+
+for x in falling_squares(10):
+    print(x)
